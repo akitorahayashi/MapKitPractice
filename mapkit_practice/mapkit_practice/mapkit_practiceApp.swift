b270301bd5645b66dd00e3609189baa05ab40c65
林明虎 <hayashi20030703@gmail.com>
2024-11-10 12:23:39 +0900

    Initial commit

---
+++ mapkit_practice/mapkit_practice/mapkit_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapkit_practiceApp.swift
+//  mapkit_practice
+//
+//  Created by 林 明虎 on 2024/11/10.
+//
+
+import SwiftUI
+
+@main
+struct mapkit_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
